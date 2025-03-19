9a63dbf640f1b109d63e209428c8ede67edadb19
JoseIraheta234 <Jluisiraheta097@gmail.com>
2025-03-19 09:22:27 -0600

    app

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ import modelsRoute from "./src/routes/models.js"
 import vehiclesRoute from "./src/routes/vehicles.js"
 import branchesRoute from "./src/routes/branches.js"
 import stockRoute from "./src/routes/stock.js"
+import RoleRoute from "./src/routes/Role.js"
+import EmployeesRoute from "./src/routes/employees.js";
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use("/api/models", modelsRoute);
 app.use("/api/vehicles", vehiclesRoute);
 app.use("/api/branches", branchesRoute);
 app.use("/api/stock", stockRoute);
+app.use("/api/Role", RoleRoute);
+app.use("/api/Employees", EmployeesRoute)
 
 
 
