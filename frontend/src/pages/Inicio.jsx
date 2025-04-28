2e389e61ec4157f87d02bfea819a3a02f7610b26
AmarisOsorio <amarisosorio10@gmail.com>
2025-04-28 11:25:42 -0600

    sdajf

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,10 +1,14 @@
 function Inicio() {
     return (
       <>
-     
-       <h1>Hola</h1>
-       <p>lalalalalala</p>
-       
+      <div>
+             <header className="header-contact">
+                <div id="header-contact-text">
+                    <h1>Luxury Drive</h1>
+                    <h3>“Domina la carretera con clase”</h3>
+                </div>
+             </header>
+      </div>
       </>
     );
   }
